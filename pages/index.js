6a4773478a22f97d26dd97bbe4cfa4088c49b12c
~litpub <litpub@cryptocculture.com>
2020-03-02 18:39:31 -0800

    Button

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,16 @@ const Home = () => (
         </h1>
 
         <h2 className="md:text-4xl text-2xl font-bold leading-loose">
-          Infrastructure for Urbit &amp; IPFS 
+          Infrastructure for Urbit &amp; IPFS
         </h2>
         
-        <p className="md:text-xl font-thin leading-relaxed max-w-md mx-auto">
+        <p className="md:text-xl font-thin leading-relaxed max-w-md mx-auto mb-10">
           From Old Norse <em>Ur&eth;r</em>, from Proto-Germanic <em>*wurdiz</em> ("fate, destiny"), Proto-Indo-European <em>*wert-</em> ("to turn, wind"). Cognate with English <em>weird.</em>
         </p>
+
+        <button class="bg-transparent hover:bg-white-500 text-white font-semibold hover:text-white py-2 px-4 border border-white rounded cursor-not-allowed">
+          Buy Urbit Planet (soon)
+        </button>
       </section>
       <section className="flex-1">
         <img className="hero-logo w-1/2 lg:w-2/5 mx-auto mt-20" src="/urth.png" alt="urth.systems" />
