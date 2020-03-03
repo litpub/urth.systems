b9f92e09ec99e8f1ab779a25c1a46d3450c59248
~litpub <litpub@cryptocculture.com>
2020-03-03 13:48:59 -0800

    Fixed plan ID

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     STRIPE_PUBLISHABLE_KEY: 'pk_live_TnUIpUWRe1U7TmaxjBtx4Ztr00IZdwf5Ds',
-    STRIPE_PLAN_ID: 'plan_GqNjwLhPjjMpTU'
+    STRIPE_PLAN_ID: 'plan_GqPN5mTu3Y9sTI'
   }
 };
