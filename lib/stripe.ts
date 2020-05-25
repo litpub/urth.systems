c79acc5c3d39d745609c40fcf5b56b7e75dda4e7
~litpub <litpub@cryptocculture.com>
2020-05-25 19:34:32 -0400

    Added stripe key env var (revoked the test env one)

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 // @TODO API version
-export const stripe = new Stripe('sk_test_uygTMEeN0Lfg6HytVTLAo8V300TEgfQPgV', { apiVersion: '2020-03-02' });
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2020-03-02' });
 
 export async function findCustomerByEmail(email: string) {
   const customers = await stripe.customers.list({ email })
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 module.exports = {
   env: {
     STRIPE_PUBLISHABLE_KEY: 'pk_live_TnUIpUWRe1U7TmaxjBtx4Ztr00IZdwf5Ds',
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     STRIPE_PLAN_ID: 'plan_GqPN5mTu3Y9sTI',
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     JWT_SECRET: process.env.JWT_SECRET
