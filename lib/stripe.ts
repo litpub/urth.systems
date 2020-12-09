859edcab75c97a72eb22efc63a587ac1c1d81c34
~litpub <litpub@cryptocculture.com>
2020-12-09 11:17:58 -0500

    Updated dependencies

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 // @TODO API version
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2020-03-02' });
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2020-08-27' });
 
 export async function findCustomerByEmail(email: string) {
   const customers = await stripe.customers.list({ email })
